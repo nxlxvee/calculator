e55522ffb6e9250d8fd5ebce2f785921693f3264
nxlxvee <feiny99@gmail.com>
2022-06-29 15:54:36 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+nubmer1 = int(input("Введите первое число: "));
+znak = input("Введите знак + - * / %: ");
+number2 = int(input("Введите следующее число: "));
+if znak == "+":
+        print(nubmer1+number2);
+if znak == "-":
+    print(nubmer1-number2);
+if znak == "*":
+    print(nubmer1*number2);
+if znak == "/":
+    print(nubmer1/number2);
+if znak == "%":
+    print(nubmer1%number2);
